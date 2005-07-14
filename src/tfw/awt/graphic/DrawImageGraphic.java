f5b25cacf5805bf9032b10695e6bc3d5463e56df
Dave <jdcove2@users.sourceforge.net>
2005-07-14 16:48:49 +0000

    Initial Version.

---
+++ src/tfw/awt/graphic/DrawImageGraphic.java
@@ -0,0 +1,68 @@
+/*
+ * The Framework Project
+ * Copyright (C) 2005 Anonymous
+ * 
+ * This library is free software; you can
+ * redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY;
+ * witout even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU
+ * Lesser General Public License along with this
+ * library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA
+ */
+package tfw.awt.graphic;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+
+public final class DrawImageGraphic
+{
+	private DrawImageGraphic() {}
+	
+	public static Graphic create(Graphic graphic, Image img, int x, int y,
+		ImageObserver observer)
+	{
+		return(new MyGraphic(graphic, img, x, y, observer));
+	}
+	
+	private static class MyGraphic implements Graphic
+	{
+		private final Graphic graphic;
+		private final Image img;
+		private final int x;
+		private final int y;
+		private final ImageObserver observer;
+		
+		public MyGraphic(Graphic graphic, Image img, int x, int y,
+			ImageObserver observer)
+		{
+			this.graphic = graphic;
+			this.img = img;
+			this.x = x;
+			this.y = y;
+			this.observer = observer;
+		}
+		
+		public void paint(Graphics2D graphics2D)
+		{
+			if (graphic != null)
+			{
+				graphic.paint(graphics2D);
+			}
+			graphics2D.drawImage(img, x, y, observer);
+		}
+	}
+}
---
+++ src/tfw/awt/graphic/SetColorGraphic.java
@@ -0,0 +1,59 @@
+/*
+ * The Framework Project
+ * Copyright (C) 2005 Anonymous
+ * 
+ * This library is free software; you can
+ * redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY;
+ * witout even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU
+ * Lesser General Public License along with this
+ * library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA
+ */
+package tfw.awt.graphic;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public final class SetColorGraphic
+{
+	private SetColorGraphic() {}
+	
+	public static Graphic create(Graphic graphic, Color color)
+	{
+		return(new MyGraphic(graphic, color));
+	}
+	
+	private static class MyGraphic implements Graphic
+	{
+		private final Graphic graphic;
+		private final Color color;
+		
+		public MyGraphic(Graphic graphic, Color color)
+		{
+			this.graphic = graphic;
+			this.color = color;
+		}
+		
+		public void paint(Graphics2D graphics2D)
+		{
+			if (graphic != null)
+			{
+				graphic.paint(graphics2D);
+			}
+			graphics2D.setColor(color);
+		}
+	}
+}
