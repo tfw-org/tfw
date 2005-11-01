8f46fdd99d28febf055e50fd754843e5450f90ca
Steve <sjcolom@users.sourceforge.net>
2005-11-01 00:56:06 +0000

    Added call to clear() in the create() method to avoid terminator and translator reuse problem

--- src/tfw/tsm/BranchFactory.java
+++ src/tfw/tsm/BranchFactory.java
@@ -24,7 +24,6 @@
  */
 package tfw.tsm;
 
-
 import tfw.check.Argument;
 import tfw.tsm.ecd.EventChannelDescription;
 
@@ -35,7 +34,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-
 
 /**
  * A factory for creating an {@link Branch}.
@@ -88,12 +86,14 @@ public class BranchFactory extends BaseBranchFactory
 
     /**
      * Adds translator between the specified parent and child event channels.
-     *
-     * @param childEventChannel the child event channel.
-     * @param parentEventChannel the parent event channel.
+     * 
+     * @param childEventChannel
+     *            the child event channel.
+     * @param parentEventChannel
+     *            the parent event channel.
      */
     public void addTranslation(EventChannelDescription childEventChannel,
-        EventChannelDescription parentEventChannel)
+            EventChannelDescription parentEventChannel)
     {
         Argument.assertNotNull(childEventChannel, "childEventChannel");
         Argument.assertNotNull(parentEventChannel, "parentEventChannel");
@@ -101,73 +101,78 @@ public class BranchFactory extends BaseBranchFactory
         if (isTerminated(childEventChannel))
         {
             throw new IllegalStateException(
-                "Attempt to translate the event channel '" +
-                childEventChannel.getEventChannelName() +
-                "' which is already terminated.");
+                    "Attempt to translate the event channel '"
+                            + childEventChannel.getEventChannelName()
+                            + "' which is already terminated.");
         }
 
         if (isTranslated(childEventChannel))
         {
             throw new IllegalStateException(
-                "Attempt to translate the event channel '" +
-                childEventChannel.getEventChannelName() +
-                "' which is already translated.");
+                    "Attempt to translate the event channel '"
+                            + childEventChannel.getEventChannelName()
+                            + "' which is already translated.");
         }
 
-		if (!childEventChannel.getConstraint().isCompatable(parentEventChannel.getConstraint()))
-		{
-			throw new IllegalArgumentException(
-				"Incompatible event channels, values from the parent event channel '" +
-				parentEventChannel.getEventChannelName() +
-				"' are not assignable to the child event channel '" +
-				childEventChannel.getEventChannelName() + "'");
-		}
-		if (!parentEventChannel.getConstraint().isCompatable(childEventChannel.getConstraint()))
-		{
-			throw new IllegalArgumentException(
-				"Incompatible event channels values from the child event channel '" +
-				childEventChannel.getEventChannelName() +
-				"' are not assignable to the parent event channel '" +
-				parentEventChannel.getEventChannelName() + "'");
-		}
+        if (!childEventChannel.getConstraint().isCompatable(
+                parentEventChannel.getConstraint()))
+        {
+            throw new IllegalArgumentException(
+                    "Incompatible event channels, values from the parent event channel '"
+                            + parentEventChannel.getEventChannelName()
+                            + "' are not assignable to the child event channel '"
+                            + childEventChannel.getEventChannelName() + "'");
+        }
+        if (!parentEventChannel.getConstraint().isCompatable(
+                childEventChannel.getConstraint()))
+        {
+            throw new IllegalArgumentException(
+                    "Incompatible event channels values from the child event channel '"
+                            + childEventChannel.getEventChannelName()
+                            + "' are not assignable to the parent event channel '"
+                            + parentEventChannel.getEventChannelName() + "'");
+        }
 
         try
         {
             translators.put(childEventChannel.getEventChannelName(),
-                new Translator(childEventChannel, parentEventChannel));
+                    new Translator(childEventChannel, parentEventChannel));
         }
         catch (ValueException unexpected)
         {
             throw new RuntimeException(
-                "Unexpected error creating a translator: " +
-                unexpected.getMessage(), unexpected);
+                    "Unexpected error creating a translator: "
+                            + unexpected.getMessage(), unexpected);
         }
     }
 
     /**
-     *  Adds a terminator for the specified event channel. A terminator is
-     * the point within the component tree structure where an event channel
-     * stops. Events from sources or publishers move up through the component
-     * hierarchy until they reach the terminator. If the event represents a
-     * state change as defined by the {@link StateChangeRule} the terminator
-     * reflects the event back down through the component tree structure to
-     * the sinks or subscribers.
-     *
-     * @param eventChannelDescription The event channel to be terminated.
-     * @param initialState The initial value to be assigned to the event
-     * channel.
-     * @param rule The state change rule for the event channel.
+     * Adds a terminator for the specified event channel. A terminator is the
+     * point within the component tree structure where an event channel stops.
+     * Events from sources or publishers move up through the component hierarchy
+     * until they reach the terminator. If the event represents a state change
+     * as defined by the {@link StateChangeRule} the terminator reflects the
+     * event back down through the component tree structure to the sinks or
+     * subscribers.
+     * 
+     * @param eventChannelDescription
+     *            The event channel to be terminated.
+     * @param initialState
+     *            The initial value to be assigned to the event channel.
+     * @param rule
+     *            The state change rule for the event channel.
      */
-    public void addEventChannel(EventChannelDescription eventChannelDescription,
-        Object initialState, StateChangeRule rule) throws ValueException
+    public void addEventChannel(
+            EventChannelDescription eventChannelDescription,
+            Object initialState, StateChangeRule rule) throws ValueException
     {
-        if ((eventChannelDescription != null) &&
-            isTranslated(eventChannelDescription))
+        if ((eventChannelDescription != null)
+                && isTranslated(eventChannelDescription))
         {
             throw new IllegalStateException(
-                "Attemp to terminate an event channel, '" +
-                eventChannelDescription.getEventChannelName() +
-                "', which is already transalated.");
+                    "Attemp to terminate an event channel, '"
+                            + eventChannelDescription.getEventChannelName()
+                            + "', which is already transalated.");
         }
 
         super.addEventChannel(eventChannelDescription, initialState, rule);
@@ -188,13 +193,19 @@ public class BranchFactory extends BaseBranchFactory
     }
 
     /**
-     * Creates a {@link Branch} with the previously specified translators
-     * and terminators.
-     * @param name The name of the branch.
-     * @return A branch with the previously specified translators and terminators.
+     * Creates a {@link Branch} with the previously specified translators and
+     * terminators. Note that this method calls {@link #clear()}.
+     * 
+     * @param name
+     *            The name of the branch.
+     * @return A branch with the previously specified translators and
+     *         terminators.
      */
     public Branch create(String name)
     {
-        return new Branch(name, getSinks(), getSources(), getTerminators());
+        Branch branch = new Branch(name, getSinks(), getSources(),
+                getTerminators());
+        this.clear();
+        return branch;
     }
 }
