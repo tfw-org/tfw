775f06202b083d23661cc9837be410d78d94fce5
Steve <sjcolom@users.sourceforge.net>
2005-11-02 02:30:51 +0000

    Reformatted

--- src/tfw/tsm/AlwaysChangeRule.java
+++ src/tfw/tsm/AlwaysChangeRule.java
@@ -24,35 +24,45 @@
  */
 package tfw.tsm;
 
+import tfw.check.Argument;
+
 /**
  * A state change rule for always notifing sinks when state is published.
  */
-public class AlwaysChangeRule implements StateChangeRule {
-	/** An instance of the rule. */
-	public static final AlwaysChangeRule RULE = new AlwaysChangeRule();
+public class AlwaysChangeRule implements StateChangeRule
+{
+    /** An instance of the rule. */
+    public static final AlwaysChangeRule RULE = new AlwaysChangeRule();
 
-	/**
-	 * Hide constructor to avoid un-needed object creation. This rule is 
-	 * stateless and therefore only one instance is needed.
-	 */
-	private AlwaysChangeRule(){}
+    /**
+     * Hide constructor to avoid un-needed object creation. This rule is
+     * stateless and therefore only one instance is needed.
+     */
+    private AlwaysChangeRule()
+    {
+    }
 
-	/**
-	 * Returns an instance of the rule.
-	 * @return an instance of the rule.
-	 */
-	public static AlwaysChangeRule getInstance(){
-		return RULE;
-	}
+    /**
+     * Returns an instance of the rule.
+     * 
+     * @return an instance of the rule.
+     */
+    public static AlwaysChangeRule getInstance()
+    {
+        return RULE;
+    }
 
-
-	/**
-	 * Returns <code>true</code> reqardless of the specified values.
-	 * @param currentState the current event channel state.
-	 * @param newState the new state value for the event channel.
-	 * @return <code>true</code> reqardless of the specified values.
-	 */
-	public boolean isChange(Object currentState, Object newState) {
-		return true;
-	}
+    /**
+     * Returns <code>true</code> reqardless of the specified values.
+     * 
+     * @param currentState
+     *            the current event channel state.
+     * @param newState
+     *            the new state value for the event channel.
+     * @return <code>true</code> reqardless of the specified values.
+     */
+    public boolean isChange(Object currentState, Object newState)
+    {
+        return true;
+    }
 }
