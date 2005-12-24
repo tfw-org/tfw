c66dcebb55aebe5b7311a596e4aa00e8ebbf2617
Steve <sjcolom@users.sourceforge.net>
2005-12-24 21:03:38 +0000

    Added check for null selected item before setting event channel

--- src/tfw/swing/combobox/SelectionInitiator.java
+++ src/tfw/swing/combobox/SelectionInitiator.java
@@ -34,45 +34,54 @@ import tfw.tsm.Initiator;
 import tfw.tsm.ecd.IntegerECD;
 import tfw.tsm.ecd.ObjectECD;
 
-public class SelectionInitiator extends Initiator implements ActionListener {
-	private final ObjectECD selectedItemECD;
+public class SelectionInitiator extends Initiator implements ActionListener
+{
+    private final ObjectECD selectedItemECD;
 
-	private final IntegerECD selectedIndexECD;
+    private final IntegerECD selectedIndexECD;
 
-	private final JComboBox comboBox;
+    private final JComboBox comboBox;
 
-	public SelectionInitiator(String name, ObjectECD selectedItemECD,
-			IntegerECD selectedIndexECD, JComboBox comboBox) {
-		super("SelectionInitiator[" + name + "]", checkECDs(selectedItemECD,
-				selectedIndexECD));
+    public SelectionInitiator(String name, ObjectECD selectedItemECD,
+            IntegerECD selectedIndexECD, JComboBox comboBox)
+    {
+        super("SelectionInitiator[" + name + "]", checkECDs(selectedItemECD,
+                selectedIndexECD));
 
-		this.selectedItemECD = selectedItemECD;
-		this.selectedIndexECD = selectedIndexECD;
-		this.comboBox = comboBox;
-	}
+        this.selectedItemECD = selectedItemECD;
+        this.selectedIndexECD = selectedIndexECD;
+        this.comboBox = comboBox;
+    }
 
-	private static ObjectECD[] checkECDs(ObjectECD selectedItemECD,
-			IntegerECD selectedIndexECD) {
-		ArrayList list = new ArrayList();
-		if (selectedItemECD != null) {
-			list.add(selectedItemECD);
-		}
-		if (selectedIndexECD != null) {
-			list.add(selectedIndexECD);
-		}
-		if (list.size() == 0) {
-			throw new IllegalArgumentException(
-					"(selectedItemECD == null)&&(selectedIndexECD == null) not allowed");
-		}
-		return (ObjectECD[]) list.toArray(new ObjectECD[list.size()]);
-	}
+    private static ObjectECD[] checkECDs(ObjectECD selectedItemECD,
+            IntegerECD selectedIndexECD)
+    {
+        ArrayList list = new ArrayList();
+        if (selectedItemECD != null)
+        {
+            list.add(selectedItemECD);
+        }
+        if (selectedIndexECD != null)
+        {
+            list.add(selectedIndexECD);
+        }
+        if (list.size() == 0)
+        {
+            throw new IllegalArgumentException(
+                    "(selectedItemECD == null)&&(selectedIndexECD == null) not allowed");
+        }
+        return (ObjectECD[]) list.toArray(new ObjectECD[list.size()]);
+    }
 
-	public void actionPerformed(ActionEvent e) {
-		if (selectedItemECD != null) {
-			set(selectedItemECD, comboBox.getSelectedItem());
-		}
-		if (selectedIndexECD != null) {
-			set(selectedIndexECD, new Integer(comboBox.getSelectedIndex()));
-		}
-	}
+    public void actionPerformed(ActionEvent e)
+    {
+        if ((selectedItemECD != null) && (comboBox.getSelectedItem() != null))
+        {
+            set(selectedItemECD, comboBox.getSelectedItem());
+        }
+        if (selectedIndexECD != null)
+        {
+            set(selectedIndexECD, new Integer(comboBox.getSelectedIndex()));
+        }
+    }
 }
