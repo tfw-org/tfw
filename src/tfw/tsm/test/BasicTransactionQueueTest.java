60f7e029c93e834b6d33e99c2529c89513097524
Steve <sjcolom@users.sourceforge.net>
2006-02-08 03:42:56 +0000

    added tests for invokeAndWait() method.

--- src/tfw/tsm/test/BasicTransactionQueueTest.java
+++ src/tfw/tsm/test/BasicTransactionQueueTest.java
@@ -40,7 +40,7 @@ public class BasicTransactionQueueTest extends TestCase
 
         try
         {
-            queue.add(null);
+            queue.invokeLater(null);
             fail("add() accepted null runnable");
         }
         catch (IllegalArgumentException expected)
@@ -48,12 +48,21 @@ public class BasicTransactionQueueTest extends TestCase
             //System.out.println(expected);
         }
     }
+    private int count = 0;
     private class TestRunnable implements Runnable{
+
     	public boolean done = false;
+        public int cnt = -1;
+        public final int sleepTime;
+        
+        public TestRunnable(int sleepTime){
+            this.sleepTime = sleepTime;
+        }
     	public void run(){
 			try {
-				Thread.sleep(500);
+				Thread.sleep(this.sleepTime);
 				done = true;
+                this.cnt = ++count;
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -61,12 +70,33 @@ public class BasicTransactionQueueTest extends TestCase
     }
     public void testAddNWait(){
 		BasicTransactionQueue queue = new BasicTransactionQueue();
-		final int count = 0;
-		TestRunnable ts = new TestRunnable();
+		TestRunnable ts = new TestRunnable(500);
 		
-		queue.add(ts);
+		queue.invokeLater(ts);
 		queue.waitTilEmpty();
 
 		assertTrue("Not done after waitTilEmpty()", ts.done);
+    }
+    
+    public void testInvokeAndWait() throws Exception {
+        BasicTransactionQueue queue = new BasicTransactionQueue();
+        try{
+            queue.invokeAndWait(null);
+            fail("invokeAndWait() accepted null");
+        } catch (IllegalArgumentException expected){
+            //System.out.println(expected);
+        }
+        TestRunnable tr1 = new TestRunnable(20);
+        TestRunnable tr2 = new TestRunnable(0);
+        TestRunnable tr3 = new TestRunnable(5);
+        queue.invokeLater(tr1);
+        queue.invokeAndWait(tr2);
+        assertEquals("tr2.cnt",2, tr2.cnt);
+        queue.invokeLater(tr3);
+        queue.waitTilEmpty();
+        
+        assertEquals("tr1.cnt",1, tr1.cnt);
+        assertEquals("tr2.cnt",2, tr2.cnt);
+        assertEquals("tr3.cnt",3, tr3.cnt);
     }
 }
