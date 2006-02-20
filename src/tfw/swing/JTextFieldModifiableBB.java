d36375ade198cd7a0701e160e06582fd8bebd5d1
Steve <sjcolom@users.sourceforge.net>
2006-02-20 22:54:02 +0000

    Fixed bug with null enabledECD.

--- src/tfw/swing/JTextFieldModifiableBB.java
+++ src/tfw/swing/JTextFieldModifiableBB.java
@@ -25,7 +25,9 @@
 package tfw.swing;
 
 import java.awt.Color;
+
 import tfw.awt.event.ActionInitiator;
+import tfw.check.Argument;
 import tfw.component.EventChannelCopyConverter;
 import tfw.tsm.Branch;
 import tfw.tsm.Commit;
@@ -36,81 +38,105 @@ import tfw.tsm.ecd.StringECD;
 
 public class JTextFieldModifiableBB extends JTextFieldBB
 {
-	private final Color defaultDisabledBackground;
-	private final Color defaultEnabledBackground;
-	private final Color modifiedBackground = new Color(153, 153, 204);
+    private final Color defaultDisabledBackground;
 
-	public JTextFieldModifiableBB(String name, StringECD textECD,
-			StringECD textAdjECD, BooleanECD enabledECD,
-			StatelessTriggerECD applyECD)
-	{
-		this(new Branch("JTextFieldModifiableBB["+name+"]"),
-			textECD, textAdjECD, enabledECD, applyECD);
-	}
+    private final Color defaultEnabledBackground;
 
-	public JTextFieldModifiableBB(Branch branch, StringECD textECD,
-			StringECD textAdjECD, BooleanECD enabledECD,
-			StatelessTriggerECD applyECD)
-	{
-		super(branch, textAdjECD, enabledECD);
-		
-		setEnabled(true);
-		defaultEnabledBackground = getBackground();
-		setEnabled(false);
-		defaultDisabledBackground = getBackground();
+    private final Color modifiedBackground = new Color(153, 153, 204);
 
-		ActionInitiator actionInitiator = new ActionInitiator(
-			"JTextFieldModifiableBB", applyECD);
-		addActionListenerToBoth(actionInitiator);
-		
-		branch.add(new ForegroundBackgroundHandler(
-			branch.getName(), textECD, textAdjECD, enabledECD));
-		branch.add(new EventChannelCopyConverter(
-			branch.getName(), textECD, textAdjECD));
-	}
+    public JTextFieldModifiableBB(String name, StringECD textECD,
+            StringECD textAdjECD, BooleanECD enabledECD,
+            StatelessTriggerECD applyECD)
+    {
+        this(new Branch("JTextFieldModifiableBB[" + name + "]"), textECD,
+                textAdjECD, enabledECD, applyECD);
+    }
 
-	/**
-	 * Sets the foreground and background state of the text field. Sets the
-	 * value of the text field if the text event channel is set during the state
-	 * change cycle.
-	 */
-	private final class ForegroundBackgroundHandler extends Commit
-	{
-		private final StringECD textName;
-		private final StringECD textAdjName;
-		private final BooleanECD enableName;
+    public JTextFieldModifiableBB(Branch branch, StringECD textECD,
+            StringECD textAdjECD, BooleanECD enabledECD,
+            StatelessTriggerECD applyECD)
+    {
+        super(branch, textAdjECD, enabledECD);
 
-		public ForegroundBackgroundHandler(String name, StringECD textSink,
-				StringECD textAdjSink, BooleanECD enableSink)
-		{
-			super("ForegroundBackgroundHandler[" + name + "]",
-				new ObjectECD[] {textSink, textAdjSink,
-					enableSink });
-			
-			this.textName = textSink;
-			this.textAdjName = textAdjSink;
-			this.enableName = enableSink;
-		}
+        setEnabled(true);
+        defaultEnabledBackground = getBackground();
+        setEnabled(false);
+        defaultDisabledBackground = getBackground();
 
-		protected void commit()
-		{
-			String text = (String) get(textName);
-			String textAdj = (String) get(textAdjName);
+        ActionInitiator actionInitiator = new ActionInitiator(
+                "JTextFieldModifiableBB", applyECD);
+        addActionListenerToBoth(actionInitiator);
 
-			if (!text.equals(textAdj))
-			{
-				setBackground(modifiedBackground);
-			}
-			else if (((Boolean) get(enableName)).booleanValue())
-			{
-				setBackground(defaultEnabledBackground);
-				setForeground(Color.black);
-			}
-			else
-			{
-				setBackground(defaultDisabledBackground);
-				setForeground(Color.black);
-			}
-		}
-	}
+        branch.add(new ForegroundBackgroundHandler(branch.getName(), textECD,
+                textAdjECD, enabledECD));
+        branch.add(new EventChannelCopyConverter(branch.getName(), textECD,
+                textAdjECD));
+    }
+
+    /**
+     * Sets the foreground and background state of the text field. Sets the
+     * value of the text field if the text event channel is set during the state
+     * change cycle.
+     */
+    private final class ForegroundBackgroundHandler extends Commit
+    {
+        private final StringECD textName;
+
+        private final StringECD textAdjName;
+
+        private final BooleanECD enableName;
+
+        public ForegroundBackgroundHandler(String name, StringECD textSink,
+                StringECD textAdjSink, BooleanECD enableSink)
+        {
+            super("ForegroundBackgroundHandler[" + name + "]", new ObjectECD[] {
+                    textSink, textAdjSink, enableSink });
+
+            this.textName = textSink;
+            this.textAdjName = textAdjSink;
+            this.enableName = enableSink;
+        }
+
+        private ObjectECD[] toArray(StringECD textSink, StringECD textAdjSink,
+                BooleanECD enableSink)
+        {
+            Argument.assertNotNull(textSink, "textSink");
+            Argument.assertNotNull(textAdjSink, "textAdjSink");
+            
+            if (enableSink == null)
+            {
+                return new ObjectECD[] { textSink, textAdjSink};
+            }
+            else
+            {
+                return new ObjectECD[] { textSink, textAdjSink, enableSink };
+            }
+        }
+
+        protected void commit()
+        {
+            String text = (String) get(textName);
+            String textAdj = (String) get(textAdjName);
+
+            boolean enabled = true;
+            if (this.enableName != null){
+                enabled = ((Boolean) get(enableName)).booleanValue();
+            } 
+            
+            if (!text.equals(textAdj))
+            {
+                setBackground(modifiedBackground);
+            }
+            else if (enabled)
+            {
+                setBackground(defaultEnabledBackground);
+                setForeground(Color.black);
+            }
+            else
+            {
+                setBackground(defaultDisabledBackground);
+                setForeground(Color.black);
+            }
+        }
+    }
 }
