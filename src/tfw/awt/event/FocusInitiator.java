b56686219b8f4b7962b6d169fa30ad34c6c3e367
Dave <jdcove2@users.sourceforge.net>
2006-03-06 04:01:13 +0000

    Initial Version.

---
+++ src/tfw/awt/event/FocusInitiator.java
@@ -0,0 +1,54 @@
+/*
+ * The Framework Project
+ * Copyright (C) 2006 Anonymous
+ * 
+ * This library is free software; you can
+ * redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY;
+ * witout even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU
+ * Lesser General Public License along with this
+ * library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA
+ */
+package tfw.awt.event;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import tfw.tsm.Initiator;
+import tfw.tsm.ecd.BooleanECD;
+import tfw.tsm.ecd.EventChannelDescription;
+
+public class FocusInitiator extends Initiator implements FocusListener
+{
+	private final BooleanECD hasFocusECD;
+	
+	public FocusInitiator(String name, BooleanECD hasFocusECD)
+	{
+		super("FocusInitiator["+name+"]",
+			new EventChannelDescription[] {hasFocusECD});
+		
+		this.hasFocusECD = hasFocusECD;
+	}
+	
+	public void focusGained(FocusEvent e)
+	{
+		set(hasFocusECD, Boolean.TRUE);
+	}
+	
+	public void focusLost(FocusEvent e)
+	{
+		set(hasFocusECD, Boolean.FALSE);
+	}
+}
