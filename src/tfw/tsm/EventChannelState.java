cd6f817043e64e8c892f396731b9565db48ddb73
Steve <sjcolom@users.sourceforge.net>
2006-04-17 23:21:27 +0000

    Made serializable.

--- src/tfw/tsm/EventChannelState.java
+++ src/tfw/tsm/EventChannelState.java
@@ -24,6 +24,8 @@
  */
 package tfw.tsm;
 
+import java.io.Serializable;
+
 import tfw.check.Argument;
 import tfw.tsm.ecd.ObjectECD;
 import tfw.value.ValueException;
@@ -31,92 +33,95 @@ import tfw.value.ValueException;
 /**
  * A container for event channel state.
  */
-public class EventChannelState
+public class EventChannelState implements Serializable
 {
-    private final ObjectECD ecd;
+    private static final long serialVersionUID = -8848929104594047886L;
+    
+	private final String eventChannelName;
 
-    private final Object state;
+	private final Object state;
 
-    /**
-     * Creates an event channel state with the specified values
-     * 
-     * @param ecd
-     *            the description of the event channel.
-     * @param state
-     *            the state of the event channel.
-     * @throws ValueException
-     *             if the specified value is not compatible with the constraint
-     *             defined in the specified event channel description.
-     */
-    public EventChannelState(ObjectECD ecd, Object state) throws ValueException
-    {
-        Argument.assertNotNull(ecd, "ecd");
-        ecd.getConstraint().checkValue(state);
-        this.ecd = ecd;
-        this.state = state;
-    }
+	/**
+	 * Creates an event channel state with the specified values
+	 * 
+	 * @param ecd
+	 *            the description of the event channel.
+	 * @param state
+	 *            the state of the event channel.
+	 * @throws ValueException
+	 *             if the specified value is not compatible with the constraint
+	 *             defined in the specified event channel description.
+	 */
+	public EventChannelState(ObjectECD ecd, Object state) throws ValueException
+	{
+		Argument.assertNotNull(ecd, "ecd");
+		ecd.getConstraint().checkValue(state);
+		this.state = state;
+		this.eventChannelName = ecd.getEventChannelName();
+	}
 
-    /**
-     * Returns the event channel description.
-     * 
-     * @return the event channel description.
-     */
-    public ObjectECD getECD()
-    {
-        return this.ecd;
-    }
+	/**
+	 * Returns the event channel description.
+	 * 
+	 * @return the event channel description.
+	 */
+	public String getEventChannelName()
+	{
+		return this.eventChannelName;
+	}
 
-    /**
-     * Returns the event channel state.
-     * 
-     * @return the event channel state.
-     */
-    public Object getState()
-    {
-        return this.state;
-    }
+	/**
+	 * Returns the event channel state.
+	 * 
+	 * @return the event channel state.
+	 */
+	public Object getState()
+	{
+		return this.state;
+	}
 
-    /**
-     * Returns a hash code value for this event channel state.
-     * 
-     * @return a hash code value for this event channel state.
-     */
-    public int hashCode()
-    {
-        return state.hashCode() + ecd.hashCode();
-    }
+	/**
+	 * Returns a hash code value for this event channel state.
+	 * 
+	 * @return a hash code value for this event channel state.
+	 */
+	public int hashCode()
+	{
+		return state.hashCode() + eventChannelName.hashCode();
+	}
 
-    /**
-     * Returns <code>true</code> if the object is and instance of
-     * <code>EventChannelState</code> and each of its attributes are equal to
-     * the attributes of this object.
-     * 
-     * @return <code>true</code> if the object is and instance of
-     *         <code>EventChannelState</code> and each of its attributes are
-     *         equal to the attributes of this object.
-     */
-    public boolean equals(Object object)
-    {
-        if (!(object instanceof EventChannelState))
-        {
-            return false;
-        }
-        EventChannelState ecs = (EventChannelState) object;
+	/**
+	 * Returns <code>true</code> if the object is and instance of
+	 * <code>EventChannelState</code> and each of its attributes are equal to
+	 * the attributes of this object.
+	 * 
+	 * @return <code>true</code> if the object is and instance of
+	 *         <code>EventChannelState</code> and each of its attributes are
+	 *         equal to the attributes of this object.
+	 */
+	public boolean equals(Object object)
+	{
+		if (!(object instanceof EventChannelState))
+		{
+			return false;
+		}
+		EventChannelState ecs = (EventChannelState) object;
 
-        return ecd.equals(ecs.ecd) && state.equals(ecs.state);
-    }
+		return eventChannelName.equals(ecs.eventChannelName)
+				&& state.equals(ecs.state);
+	}
 
-    /**
-     * Returns a string representation of this event channel state.
-     * 
-     * @return a string representation of this event channel state.
-     */
-    public String toString()
-    {
-        StringBuffer sb = new StringBuffer();
-        sb.append("EventChannelState[");
-        sb.append(ecd.getEventChannelName()).append(" = ").append(state);
-        sb.append("]");
-        return sb.toString();
-    }
+	/**
+	 * Returns a string representation of this event channel state.
+	 * 
+	 * @return a string representation of this event channel state.
+	 */
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append("EventChannelState[");
+		sb.append(eventChannelName).append(" = ").append(state);
+		sb.append("]");
+		return sb.toString();
+	}
 }
