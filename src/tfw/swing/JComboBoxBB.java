7dfffeedd676b3c82da2e9de3c235cf2d44a5a2f
Mike J. Bell <ckimyt@users.sourceforge.net>
2009-12-31 03:33:06 +0000

    fix spelling "witout" -> without in GPL notices

--- src/tfw/swing/JComboBoxBB.java
+++ src/tfw/swing/JComboBoxBB.java
@@ -7,7 +7,7 @@
  * any later version.
  * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; witout even the implied warranty of MERCHANTABILITY or FITNESS
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  * 
--- src/tfw/tsm/test/StateChangeCycleDelayTest.java
+++ src/tfw/tsm/test/StateChangeCycleDelayTest.java
@@ -11,7 +11,7 @@
  *
  * This library is distributed in the hope that it
  * will be useful, but WITHOUT ANY WARRANTY;
- * witout even the implied warranty of
+ * without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE.  See the GNU Lesser General Public
  * License for more details.
