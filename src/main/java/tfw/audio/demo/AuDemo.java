44448aec2e3c3f5af4c626c326569c67de711da5
jdcove2 <59747923+jdcove2@users.noreply.github.com>
2026-01-30 21:42:01 +0000

    Added testing for AuDemo.

--- src/main/java/tfw/audio/demo/AuDemo.java
+++ src/main/java/tfw/audio/demo/AuDemo.java
@@ -9,7 +9,6 @@ import tfw.immutable.ila.byteila.ByteIla;
 import tfw.immutable.ila.byteila.ByteIlaFromFile;
 import tfw.immutable.ila.byteila.ByteIlaUtil;
 import tfw.immutable.ila.doubleila.DoubleIla;
-import tfw.immutable.ila.doubleila.DoubleIlaUtil;
 
 public class AuDemo {
     private static final FluentLogger LOGGER = FluentLogger.forEnclosingClass();
@@ -23,13 +22,13 @@ public class AuDemo {
             return;
         }
 
-        File file = new File(args[0]);
-        ByteIla byteIla = ByteIlaFromFile.create(file);
-        Au auFileFormat = new Au(byteIla);
+        final File file = new File(args[0]);
+        final ByteIla byteIla = ByteIlaFromFile.create(file);
+        final Au auFileFormat = new Au(byteIla);
 
         LOGGER.atInfo().log("filename = %s", args[0]);
         LOGGER.atInfo().log("file.length = %d", file.length());
-        LOGGER.atInfo().log("magicNumber = %x", auFileFormat.magicNumber);
+        LOGGER.atInfo().log("magicNumber = %X", auFileFormat.magicNumber);
         LOGGER.atInfo().log("offset = %d", auFileFormat.offset);
         LOGGER.atInfo().log("dataSize = %d", auFileFormat.dataSize);
         LOGGER.atInfo().log("encoding = %d", auFileFormat.encoding);
@@ -38,16 +37,9 @@ public class AuDemo {
         LOGGER.atInfo().log("annotation = %s", new String(ByteIlaUtil.toArray(auFileFormat.annotation)));
         LOGGER.atInfo().log("data.length = %d", auFileFormat.audioData.length());
 
-        DoubleIla normalizedData = NormalizedDoubleIlaFromAuAudioData.create(
+        final DoubleIla normalizedData = NormalizedDoubleIlaFromAuAudioData.create(
                 auFileFormat.audioData, auFileFormat.magicNumber, auFileFormat.encoding, 1000);
 
         LOGGER.atInfo().log("normalizedData.length = %d", normalizedData.length());
-
-        if (normalizedData != null) {
-            double[] d = DoubleIlaUtil.toArray(normalizedData, 0, 10);
-            for (int i = 0; i < d.length; i++) {
-                LOGGER.atInfo().log("  nD[%d]=%d", i, d[i]);
-            }
-        }
     }
 }
