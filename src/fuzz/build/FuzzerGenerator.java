ce9e7bc046d5e4ee078e1953c185ece6a2a0d021
jdcove2 <59747923+jdcove2@users.noreply.github.com>
2026-08-20 21:51:17 +0000

    First in a bunch more changes.

--- src/fuzz/build/FuzzerGenerator.java
+++ src/fuzz/build/FuzzerGenerator.java
@@ -15,86 +15,149 @@ import java.util.Map;
 
 public final class FuzzerGenerator {
 
-    private FuzzerGenerator() {}
+  private FuzzerGenerator() {}
 
-    public static void main(final String[] args) throws Exception {
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Usage: FuzzerGenerator <template-directory> <fuzz-directory>");
-        }
-
-        final Path templateDirectory = Path.of(args[0]);
-        final Path fuzzDirectory = Path.of(args[1]);
-
-        final Configuration configuration = new Configuration(Configuration.VERSION_2_3_34);
-
-        configuration.setDirectoryForTemplateLoading(templateDirectory.toFile());
-        configuration.setDefaultEncoding(StandardCharsets.UTF_8.name());
-        configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        configuration.setLogTemplateExceptions(false);
-        configuration.setWrapUncheckedExceptions(true);
-
-        final Template template = configuration.getTemplate("tfw/immutable/ilaf/IlaFactoryFromArrayFuzzer.java.ftl");
-
-        for (final Map<String, Object> model : definitions()) {
-            generate(template, model, fuzzDirectory);
-        }
+  public static void main(final String[] args) throws Exception {
+    if (args.length != 2) {
+      throw new IllegalArgumentException(
+          "Usage: FuzzerGenerator <template-directory> <fuzz-directory>");
     }
 
-    private static void generate(final Template template, final Map<String, Object> model, final Path fuzzDirectory)
-            throws Exception {
+    final Path templateDirectory = Path.of(args[0]);
+    final Path fuzzDirectory = Path.of(args[1]);
 
-        final String packageName = (String) model.get("packageName");
-        final String className = (String) model.get("className");
+    final Configuration configuration =
+        new Configuration(Configuration.VERSION_2_3_34);
 
-        final Path packageDirectory = fuzzDirectory.resolve(packageName.replace('.', File.separatorChar));
+    configuration.setDirectoryForTemplateLoading(templateDirectory.toFile());
+    configuration.setDefaultEncoding(StandardCharsets.UTF_8.name());
+    configuration.setTemplateExceptionHandler(
+        TemplateExceptionHandler.RETHROW_HANDLER);
+    configuration.setLogTemplateExceptions(false);
+    configuration.setWrapUncheckedExceptions(true);
 
-        Files.createDirectories(packageDirectory);
+    final Template template =
+        configuration.getTemplate(
+            "tfw/immutable/ilaf/IlaFactoryFromArrayFuzzer.java.ftl");
 
-        final Path outputFile = packageDirectory.resolve(className + ".java");
+    for (final Map<String, Object> model : definitions()) {
+      generate(template, model, fuzzDirectory);
+    }
+  }
 
-        try (Writer writer = Files.newBufferedWriter(outputFile, StandardCharsets.UTF_8)) {
-            template.process(model, writer);
-        }
+  private static void generate(
+      final Template template,
+      final Map<String, Object> model,
+      final Path fuzzDirectory)
+      throws Exception {
+
+    final String packageName = (String) model.get("package");
+    final String className = (String) model.get("className");
+
+    final Path packageDirectory =
+        fuzzDirectory.resolve(
+            packageName.replace('.', File.separatorChar));
+
+    Files.createDirectories(packageDirectory);
+
+    final Path outputFile =
+        packageDirectory.resolve(className + ".java");
+
+    try (Writer writer =
+        Files.newBufferedWriter(
+            outputFile,
+            StandardCharsets.UTF_8)) {
+      template.process(model, writer);
+    }
+  }
+
+  private static List<Map<String, Object>> definitions() {
+    return Arrays.asList(
+        definition(
+            "booleanilaf",
+            "BooleanIlaFactoryFromArrayFuzzer",
+            "boolean",
+            "BooleanIla",
+            null),
+
+        definition(
+            "byteilaf",
+            "ByteIlaFactoryFromArrayFuzzer",
+            "byte",
+            "ByteIla",
+            null),
+
+        definition(
+            "charilaf",
+            "CharIlaFactoryFromArrayFuzzer",
+            "char",
+            "CharIla",
+            null),
+
+        definition(
+            "doubleilaf",
+            "DoubleIlaFactoryFromArrayFuzzer",
+            "double",
+            "DoubleIla",
+            null),
+
+        definition(
+            "floatilaf",
+            "FloatIlaFactoryFromArrayFuzzer",
+            "float",
+            "FloatIla",
+            null),
+
+        definition(
+            "intilaf",
+            "IntIlaFactoryFromArrayFuzzer",
+            "int",
+            "IntIla",
+            null),
+
+        definition(
+            "longilaf",
+            "LongIlaFactoryFromArrayFuzzer",
+            "long",
+            "LongIla",
+            null),
+
+        definition(
+            "objectilaf",
+            "ObjectIlaFactoryFromArrayFuzzer",
+            "Object",
+            "ObjectIla",
+            "Object"),
+
+        definition(
+            "shortilaf",
+            "ShortIlaFactoryFromArrayFuzzer",
+            "short",
+            "ShortIla",
+            null));
+  }
+
+  private static Map<String, Object> definition(
+      final String typePackage,
+      final String className,
+      final String arrayType,
+      final String ilaType,
+      final String generic) {
+
+    final Map<String, Object> model = new HashMap<>();
+
+    model.put(
+        "package",
+        "tfw.immutable.ilaf." + typePackage);
+
+    model.put("className", className);
+    model.put("arrayType", arrayType);
+    model.put("ilaType", ilaType);
+
+    if (generic != null) {
+      model.put("generic", generic);
     }
 
-    private static List<Map<String, Object>> definitions() {
-        return Arrays.asList(
-                definition(
-                        "booleanilaf",
-                        "BooleanIlaFactoryFromArrayFuzzer",
-                        "boolean",
-                        "Boolean",
-                        "booleanila",
-                        "BooleanIla"),
-                definition("byteilaf", "ByteIlaFactoryFromArrayFuzzer", "byte", "Byte", "byteila", "ByteIla"),
-                definition("charilaf", "CharIlaFactoryFromArrayFuzzer", "char", "Char", "charila", "CharIla"),
-                definition(
-                        "doubleilaf", "DoubleIlaFactoryFromArrayFuzzer", "double", "Double", "doubleila", "DoubleIla"),
-                definition("floatilaf", "FloatIlaFactoryFromArrayFuzzer", "float", "Float", "floatila", "FloatIla"),
-                definition("intilaf", "IntIlaFactoryFromArrayFuzzer", "int", "Integer", "intila", "IntIla"),
-                definition("longilaf", "LongIlaFactoryFromArrayFuzzer", "long", "Long", "longila", "LongIla"),
-                definition(
-                        "objectilaf", "ObjectIlaFactoryFromArrayFuzzer", "Object", "Object", "objectila", "ObjectIla"),
-                definition("shortilaf", "ShortIlaFactoryFromArrayFuzzer", "short", "Short", "shortila", "ShortIla"));
-    }
-
-    private static Map<String, Object> definition(
-            final String typePackage,
-            final String className,
-            final String type,
-            final String typeName,
-            final String ilaPackage,
-            final String ilaType) {
-
-        final Map<String, Object> model = new HashMap<>();
-
-        model.put("packageName", "tfw.immutable.ilaf." + typePackage);
-        model.put("className", className);
-        model.put("type", type);
-        model.put("typeName", typeName);
-        model.put("ilaPackage", ilaPackage);
-        model.put("ilaType", ilaType);
-
-        return model;
-    }
+    return model;
+  }
 }
